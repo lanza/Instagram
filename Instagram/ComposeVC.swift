a034dfd0fadafd2f1218bb2ab97d460314b51dff
Steve Uffelman <sruffelman@gmail.com>
2016-02-12 12:40:47 -0800

    cameras

--- Instagram/ComposeVC.swift
+++ Instagram/ComposeVC.swift
@@ -2,27 +2,44 @@ import UIKit
 
 class ComposeVC: UIViewController {
     
-    // show the image chosen or taken
+    // storyboard connections
+    @IBOutlet weak var navBar: UINavigationBar!
     @IBOutlet weak var imageView: UIImageView!
-    
-    //WARNING NICK CHANGE THIS... WHY? – Nicholas
-    @IBOutlet var textFieldNickChangeThis: UITextView!
-    
-    
+    @IBOutlet weak var textView: UITextView!
+    @IBOutlet weak var cancelButton: UIBarButtonItem!
+    @IBOutlet weak var shareButton: UIBarButtonItem!
+    @IBOutlet weak var addTextButton: UIButton!
+
+    // properties
     var usersImage: UIImage?
     var imageURL: NSURL?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        setUpUI()
+
         imageView.image = usersImage
+        
+        textView.hidden = true
+        
+        cancelButton.tintColor = UIColor.whiteColor()
+        shareButton.tintColor = UIColor.whiteColor()
+        addTextButton.tintColor = UIColor.instagramColor()
+
+        self.navBar.barTintColor = UIColor.instagramColor()
+        
     }
     
+    @IBAction func onAddTextButtonTapped(sender: AnyObject) {
+        
+        textView.hidden = false
+        textView.becomeFirstResponder()
+        
+    }
     
     @IBAction func onShareTapped(sender: AnyObject) {
         
         guard let imageURL = imageURL else { return }
-        if let text = textFieldNickChangeThis.text {
+        if let text = textView.text {
             CloudManager.sharedManager.postImage(imageURL, description: text)
             self.dismissViewControllerAnimated(true, completion: nil)
         }
@@ -33,12 +50,17 @@ class ComposeVC: UIViewController {
         self.dismissViewControllerAnimated(true, completion: nil)
     }
     
-    // resign first responder:
-    func textFieldShouldReturn(textField: UITextField!) -> Bool // called when 'return' key pressed. return NO to ignore.
-    {
-        textField.resignFirstResponder()
-        return true;
-    }    //
     
+    override func viewWillAppear(animated: Bool) {
+        
+        super.viewWillAppear(animated)
+        UIApplication.sharedApplication().setStatusBarHidden(true, withAnimation: UIStatusBarAnimation.None)
+    }
+    
+    override func viewWillDisappear(animated: Bool) {
+        
+        super.viewWillDisappear(animated)
+        UIApplication.sharedApplication().setStatusBarHidden(false, withAnimation: UIStatusBarAnimation.None)
+    }
     
 }
